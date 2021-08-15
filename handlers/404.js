523ae660114c8c92a3a0891b23749a53f6c3d595
saleem-ux <saleem_diab86@yahoo.com>
2021-08-15 11:23:48 -0700

    lab01

---
+++ handlers/404.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = (req, res) => {
+    res.status(404).send({
+        error: 404,
+        route: req.path,
+        message: 'Not found'
+    });
+}
+
+// not found handler
